2ac566bbc7a2510a025f830b617d8d63f3b96d18
Aziz <azizovaa@develop.mos.ru>
2025-10-06 11:07:51 +0300

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: true,
   modules: ['@nuxtjs/google-fonts', '@nuxt/image'],
   css: ['@/assets/main.scss', '@/assets/client.scss'],
   app: {

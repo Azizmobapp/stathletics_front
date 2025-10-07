113c365b5d7a5f3ae3c4e02625242b198cb900d6
Aziz <azizovaa@develop.mos.ru>
2025-10-07 19:55:51 +0300

    1

--- app/pages/athletes/index.vue
+++ app/pages/athletes/index.vue
@@ -4,34 +4,72 @@
       <div class="athletes">
         <table>
           <thead>
-            <tr>
-              <th>ФОТО</th>
-              <th>ФИО</th>
-              <th>ГОРОД</th>
-              <th>
-                <NuxtImg class="table-medal" src="/images/gold.png" alt="Золото" />
-              </th>
-              <th>
-                <NuxtImg class="table-medal" src="/images/silver.png" alt="Серебро" />
-              </th>
-              <th>
-                <NuxtImg class="table-medal" src="/images/bronze.png" alt="Бронза" />
-              </th>
-              <th>ИТОГО</th>
-            </tr>
+          <tr>
+            <th>ФОТО</th>
+            <th>ФИО</th>
+            <th>ГОРОД</th>
+            <th>
+              <NuxtImg class="table-medal" src="/images/gold.png" alt="Золото"/>
+            </th>
+            <th>
+              <NuxtImg class="table-medal" src="/images/silver.png" alt="Серебро"/>
+            </th>
+            <th>
+              <NuxtImg class="table-medal" src="/images/bronze.png" alt="Бронза"/>
+            </th>
+            <th>ИТОГО</th>
+          </tr>
           </thead>
           <tbody>
-            <tr v-for="athlete in athletesData">
-              <td class="photo-td"><div class="athlete-photo"></div></td>
-              <td>{{ athlete.name }}</td>
-              <td>{{ athlete.city }}</td>
-              <td>{{ athlete.gold }}</td>
-              <td>{{ athlete.silver }}</td>
-              <td>{{ athlete.bronze }}</td>
-              <td>{{ athlete.total }}</td>
-            </tr>
+          <tr v-for="athlete in athletesData">
+            <td class="photo-td">
+              <div class="athlete-photo"></div>
+            </td>
+            <td>{{ athlete.name }}</td>
+            <td>{{ athlete.city }}</td>
+            <td>{{ athlete.gold }}</td>
+            <td>{{ athlete.silver }}</td>
+            <td>{{ athlete.bronze }}</td>
+            <td>{{ athlete.total }}</td>
+          </tr>
           </tbody>
         </table>
+      </div>
+
+    </div>
+    <div class="b-athletes">
+      <div class="athletes athletes-card-list">
+        <div v-for="athlete in athletesData" class="athlete-card">
+          <div class="photo-div">
+            <div class="athlete-photo"></div>
+          </div>
+          <div>
+            <div class="athlete-card-info">
+              <h3>{{ athlete.name }}</h3>
+              <p>Город: {{ athlete.city }}</p>
+            </div>
+            <div class="athlete-card-medals">
+              <div>
+                <div>
+                  <NuxtImg class="table-medal" src="/images/gold.png" alt="Золото"/>
+                </div>
+                <p>{{ athlete.gold }}</p>
+              </div>
+              <div>
+                <div>
+                  <NuxtImg class="table-medal" src="/images/silver.png" alt="Серебро"/>
+                </div>
+                <p>{{ athlete.silver }}</p>
+              </div>
+              <div>
+                <div>
+                  <NuxtImg class="table-medal" src="/images/bronze.png" alt="Бронза"/>
+                </div>
+                <p>{{ athlete.bronze }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
